9ff602bbb98d394339da0d00ff9fec8eaec59372
thais <thais@NOTE_THATA>
2026-08-26 19:55:07 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine(">>> Entrando Middleware 1");
+    await next();
+    Console.WriteLine("<<< Saindo Middleware 1");
+});
+app.Use(async (context, next) =>
+{
+    Console.WriteLine("     >>> Entrando Middleware 2");
+    if(context.Request.Path == "/bloqueado")
+    {
+        context.Response.StatusCode = 403;
+        await context.Response.WriteAsync("Acesso bloqueado.");
+        return;
+    }
+    await next();
+    Console.WriteLine("     <<< Saindo Middleware 2");
+});
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
